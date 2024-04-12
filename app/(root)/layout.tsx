951d24ff202a619035b6aa86a68edbbdef31e1dc
thesumitsuryawanshi <thesumit0013@gmail.com>
2024-04-12 15:13:23 +0530

    deployment issue resolved

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+
 import StreamVideoProvider from '@/providers/StreamClientProvider';
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
